c028b06cef930003cdaf638b7ab3ff2189ecbc99
Carlos Larios <20460440@colima.tecnm.mx>
2024-03-12 14:28:13 -0600

    Cambio a resultado float

--- src/App.jsx
+++ src/App.jsx
@@ -5,39 +5,38 @@ function App() {
   const [result, setResult] = useState(0)
   const [value1, setValue1] = useState('')
   const [value2, setValue2] = useState('')
-  const [resultClass, setResultClass] = useState('') // Estado para la clase del resultado
 
 
   const handleSum = () => {
-    setResult(value1 + value2)
+    setResult(parseFloat(value1) + parseFloat(value2))
     
   }
 
   const handleSub = () => {
-    setResult(value1 - value2)
+    setResult(parseFloat(value1) - parseFloat(value2))
   }
 
   const handleMult = () => {
-    setResult(value1 * value2)
+    setResult(parseFloat(value1) * parseFloat(value2) )
   }
 
   const handleDiv = () => {
-    setResult(value1 / value2)
+    setResult(parseFloat(value1) / parseFloat(value2))  
   }
 
   return (
     <div className="calculator">
       <h1>Calculadora</h1>
 
-      <input type="text" value={value1} onChange={(e) => setValue1(Number(e.target.value))} />
-      <input type="text" value={value2} onChange={(e) => setValue2(Number(e.target.value))} />
+      <input type="text" value={value1} onChange={(e) => setValue1((e.target.value))} />
+      <input type="text" value={value2} onChange={(e) => setValue2((e.target.value))} />
       <div className="botones">
-      <button className="operation-button" onClick={() => { handleSum(); setResultClass('animated'); }}>+</button>
-      <button className="operation-button" onClick={() => { handleSub(); setResultClass('animated'); }}>-</button>
-      <button className="operation-button" onClick={() => { handleMult(); setResultClass('animated'); }}>*</button>
-      <button className="operation-button" onClick={() => { handleDiv(); setResultClass('animated'); }}>/</button>
+      <button className="operation-button" onClick={() => { handleSum(); }}>+</button>
+      <button className="operation-button" onClick={() => { handleSub(); }}>-</button>
+      <button className="operation-button" onClick={() => { handleMult(); }}>*</button>
+      <button className="operation-button" onClick={() => { handleDiv(); }}>/</button>
       </div>
-      <h2 className={`result ${resultClass}`}>Resultado: {result}</h2>
+      <h2 className="result">Resultado: {result}</h2>
 
     </div>
   )
